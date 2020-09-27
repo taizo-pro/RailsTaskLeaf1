c0be0903187dd96681e23f5c9b7cbed124bb4f76
taizo-pro <t2s.single@gmail.com>
2020-09-27 12:53:14 +0900

    can register from new create screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    # Flashに"タスクを〜"を保持させてからリダイレクトする。
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit
+  end
+
+  private
+  def task_params
+    # {task: {...}}の形かどうかチェックして、nameとdescriptionのだけを引っ張る
+    params.require(:task).permit(:name, :description)
   end
 end
